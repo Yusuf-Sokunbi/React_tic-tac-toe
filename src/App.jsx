4878f8c61a32ef5d51a2092e448ee117b334c217
Yusuf-Sokunbi <adeloaoo87@gmail.com>
2024-09-02 10:31:50 +0100

    Header of Page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+return (
+  <main>
+    <ol>
+      <li><span className='player-name'>Player1</span>
+      <span className='Player-symbol'>X</span></li>
+      <li><span className='player-name'>Player2</span>
+      <span className='Player-symbol'>O</span></li>
+    </ol>
+
+  </main>
+)
+}
+
+export default App
